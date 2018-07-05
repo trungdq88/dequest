1fec488cc2d5eedc09e9b68535c1bb75bd304b88
Trung Dinh Quang <trungdq88@gmail.com>
2018-07-05 16:52:18 +0700

    Breaking change: arguments to method is added with action

--- src/dequest.js
+++ src/dequest.js
@@ -99,7 +99,7 @@ const resolveRequest = async (api, action) => {
     if (!method) {
       throw new Error(`Dequest: method ${type} not registered in custom api`);
     }
-    return method(...action.request.args);
+    return method(action.request.args, action);
   }
   return action.request;
 };
